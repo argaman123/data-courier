360344297c6eeaef19dfd1248f8e29f3faeae729
argam <argaman.gez@gmail.com>
2026-03-15 19:54:03 +0200

    Added a total perf counter, and made pacer global

--- networking/send.py
+++ networking/send.py
@@ -34,7 +34,6 @@ class Sender:
         chunks = [file.bytes[i:i + settings.chunk_size] for i in range(0, len(file.bytes), settings.chunk_size)]
         logging.info(f"Sending {file.path} [{file.id.hex()}] ({len(chunks)} chunks)")
         self.send_packet(Packet(file.id, 0, len(chunks), file.path.encode('utf-8')))
-        next(self.pacer)
         for pass_num in range(settings.passes):
             start_time = time.perf_counter()
             for index, payload in enumerate(chunks):
@@ -45,13 +44,15 @@ class Sender:
 
 if __name__ == "__main__":
     sender = Sender()
-
+    start_time = time.perf_counter()
+    total_bytes = 0
     from pathlib import Path
     input_folder = Path(settings.input_folder)
     for filepath in input_folder.rglob('*'):
         if filepath.is_file():
             path = filepath.relative_to(input_folder)
-            sender.send_file(File(str(path), filepath.read_bytes()))
-
-    logging.info(f"Finished sending all files in {input_folder}, sending save signal")
+            file_bytes = filepath.read_bytes()
+            total_bytes += len(file_bytes)
+            sender.send_file(File(str(path), file_bytes))
+    logging.info(f"Finished sending all files in {input_folder} at {(1/((time.perf_counter() - start_time)/total_bytes))/1_000_000:.1f}MB/s, sending save signal")
     sender.send_packet(Packet(b'', 2, 0, b'')) # TODO FOR TESTING

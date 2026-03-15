a7bafc1cbce973ba96d755c5835453f5d746955e
argam <argaman.gez@gmail.com>
2026-03-15 18:33:58 +0200

    Added simple configuration file

---
+++ config.py
@@ -0,0 +1,17 @@
+import logging
+from dynaconf import Dynaconf
+
+settings = Dynaconf(
+    settings_files=["resources/settings.toml"],  # main config file
+    environments=True,                 # enable profiles
+    envvar_prefix="APP"                # optional: override via environment variables
+)
+
+
+
+# Configure it once at the top of your file
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)

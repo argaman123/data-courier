660e946c8ae995bd9d1ffd2a44d7cc10d25c755e
argam <argaman.gez@gmail.com>
2026-03-16 14:15:31 +0200

    saves payload instead of packets

--- networking/objects/partial_file.py
+++ networking/objects/partial_file.py
@@ -7,12 +7,12 @@ from networking.objects.packet import Packet
 class PartialFile:
     def __init__(self):
         self.header: Packet | None = None
-        self.chunks: dict[int, Packet] = {}
+        self.chunks: dict[int, bytes] = {}
         self.complete: bool = False
 
     def to_file(self) -> File:
         return File(self.header.payload.decode('utf-8'),
-                    b''.join(self.chunks[i].payload for i in range(len(self.chunks))), self.header.id)
+                    b''.join(self.chunks[i] for i in range(len(self.chunks))), self.header.id)
 
     def process(self, packet: Packet) -> bool:
         if self.complete: return False
@@ -21,7 +21,7 @@ class PartialFile:
             case 0:
                 self.header = packet
             case 1:
-                self.chunks[packet.index] = packet
+                self.chunks[packet.index] = packet.payload
             case 2: # TODO FOR TESTING
                 return True
 
--- networking/receive/receive.py
+++ networking/receive/receive.py
@@ -1,4 +1,3 @@
-import gc
 import logging
 import socket
 from pathlib import Path

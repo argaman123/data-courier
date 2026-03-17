e853bebd653d49b2d87dcd3b861a72dfceb575ab
argam <argaman.gez@gmail.com>
2026-03-17 16:51:49 +0200

    Added option to disable pacer

--- networking/send/pacing.py
+++ networking/send/pacing.py
@@ -5,16 +5,20 @@ from config import settings, logger
 
 class Pacer:
     def __init__(self):
-        self.start_time = time.perf_counter()
-        self.packets_sent = 0
-        self.target_batch_time = (settings.batch_size * settings.chunk_size) / settings.network_speed
-        logger.info(f"Pacer initialized {self.target_batch_time:.3f}s / {settings.batch_size} packets")
+        if not settings.pacer_target_speed:
+            self.disabled = True
+        else:
+            self.start_time = time.perf_counter()
+            self.packets_sent = 0
+            self.target_batch_time = (settings.batch_size * settings.chunk_size) / settings.pacer_target_speed
+            logger.info(f"Pacer initialized {self.target_batch_time:.3f}s / {settings.batch_size} packets")
 
     def reset(self):
         self.packets_sent = 0
         self.start_time = time.perf_counter()
 
     def wait_if_needed(self):
+        if self.disabled: return
         self.packets_sent += 1
         if self.packets_sent == settings.batch_size:
             elapsed_time = time.perf_counter() - self.start_time

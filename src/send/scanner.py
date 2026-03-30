4a3d14b629624a8bb654a978faabb5daf1273b68
argam <argaman.gez@gmail.com>
2026-03-30 14:39:02 +0300

    Now re-queues files left in temp folder before starting to scan for others

--- src/send/scanner.py
+++ src/send/scanner.py
@@ -15,7 +15,7 @@ class Scanner(Thread):
         self.input_folder = Path(settings.input_folder)
         self.temp_folder = Path(settings.temp_folder)
         self.file_changed_time = settings.file_changed_time
-        self.file_scan_delay = settings.file_scan_delay
+        self.file_scan_interval = settings.file_scan_interval
         self.queues = queues
         self.folders: list[Path] = [self.input_folder / folder for folder in queues.keys()]
         self.files = {}
@@ -55,31 +55,40 @@ class Scanner(Thread):
             if self._is_locked(file):
                 logger.debug(f"{file} is locked, skipping for now...")
                 return
-
-            target_path = self.temp_folder / str(file.relative_to(self.input_folder))
-            if target_path.exists():
-                logger.debug(f"{file} already exists in {self.temp_folder}, skipping for now...")
-                return
             
-            self._move_and_queue_file(file, target_path)
+            self._move_and_queue_file(file, self.input_folder)
             
         except FileNotFoundError:
             pass
 
-    def _move_and_queue_file(self, file: Path, target: Path):
+    def _move_file(self, file: Path, source_path: Path):
         try:
+            target = self.temp_folder / file.relative_to(source_path)
             logger.debug(f"Moving {file} to {self.temp_folder}")
+            if source_path != self.temp_folder and target.exists():
+                logger.debug(f"{file} already exists in {self.temp_folder}, skipping for now...")
+                return
             target.parent.mkdir(exist_ok=True)
             shutil.move(str(file), str(target))
-            self.queues[file.parent.name].put(str(file.relative_to(settings.input_folder)))
-            logger.info(f"Queueing {target}")
+            self._queue_file(target, self.temp_folder)
             del self.files[file]
         except (OSError, IOError) as e:
             logger.debug(f"{file} is still being modified, skipping for now... {e}")
 
+    def _queue_file(self, file: Path, source_path: Path):
+        logger.info(f"Queueing {file}")
+        self.queues[file.parent.name].put(str(file.relative_to(source_path)))
+
+    def _requeue_temp_folder(self):
+        for file in self.temp_folder.iterdir():
+            logger.info(f"Re-queueing {file}")
+            self._queue_file(file, self.temp_folder)
+
+
     def run(self):
         self.files = {}
         logger.info(f"Started scanning {self.input_folder}")
+        self._requeue_temp_folder()
         while True:
             try:
                 current_files = set()
@@ -97,6 +106,6 @@ class Scanner(Thread):
                     if path not in current_files:
                         del self.files[path]
             except RuntimeError as e:
-                logger.error(f"{e}")
+                logger.error(f"Error while scanning {self.input_folder}: {e}")
 
-            time.sleep(self.file_scan_delay)
+            time.sleep(self.file_scan_interval)
